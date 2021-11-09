1c31da6256580e5bbea0cccdcdf325ad8f54466a
vanderlindenma <vanderlindenma@users.noreply.github.com>
2021-11-09 10:15:51 -0500

    Add doc to two first functions

--- R/rand_vect.R
+++ R/rand_vect.R
@@ -1,3 +1,40 @@
+#' Random vector of N integers that sum to M
+#'
+#' @description Random vector of N integers that sum to M (https://stackoverflow.com/questions/24845909/generate-n-random-integers-that-sum-to-m-in-r)
+#' @name rand_vect
+#' @usage rand_vect(N,M)
+#' @param N Size of the vector.
+#' @param M Sum of elements in the vector.
+#' @param sd Approximate standard deviation for elements of vector (default = 1).
+#' @param pos.only Resulting vector contains only positive integers (default = TRUE)
+#' @return Vector of size \code{N} with elements summing to \code{M}
+#' @examples
+#' rand_vect(3, 50)
+#' [1] 17 16 17
+#' rand_vect(10, 10, pos.only = FALSE)
+#' [1]  0  2  3  2  0  0 -1  2  1  1
+#' rand_vect(10, 5, pos.only = TRUE)
+#' [1] 0 0 0 0 2 0 0 1 2 0
+#' rand_vect(10, -1, pos.only = FALSE)
+#' [1] -1 -1  1 -2  2  1  1  0 -1 -1
+#' @export
+rand_vect <- function(N, M, sd = 1, pos.only = TRUE) {
+  vec <- rnorm(N, M/N, sd)
+  if (abs(sum(vec)) < 0.01) vec <- vec + 1
+  vec <- round(vec / sum(vec) * M)
+  deviation <- M - sum(vec)
+  for (. in seq_len(abs(deviation))) {
+    vec[i] <- vec[i <- sample(N, 1)] + sign(deviation)
+  }
+  if (pos.only) while (any(vec < 0)) {
+    negs <- vec < 0
+    pos  <- vec > 0
+    vec[negs][i] <- vec[negs][i <- sample(sum(negs), 1)] + 1
+    vec[pos][i]  <- vec[pos ][i <- sample(sum(pos ), 1)] - 1
+  }
+  return(vec)
+}
+
 # https://stackoverflow.com/questions/24845909/generate-n-random-integers-that-sum-to-m-in-r
 # QQQQ: Generate N random integers that sum to M in R
 # I would like to generate N random positive integers that sum to M. I would like the random positive integers to be selected around a fairly normal distribution whose mean is M/N, with a small standard deviation (is it possible to set this as a constraint?).
@@ -52,21 +89,3 @@
 #
 # rand_vect(10, -1, pos.only = FALSE)
 # # [1] -1 -1  1 -2  2  1  1  0 -1 -1
-
-
-rand_vect <- function(N, M, sd = 1, pos.only = TRUE) {
-  vec <- rnorm(N, M/N, sd)
-  if (abs(sum(vec)) < 0.01) vec <- vec + 1
-  vec <- round(vec / sum(vec) * M)
-  deviation <- M - sum(vec)
-  for (. in seq_len(abs(deviation))) {
-    vec[i] <- vec[i <- sample(N, 1)] + sign(deviation)
-  }
-  if (pos.only) while (any(vec < 0)) {
-    negs <- vec < 0
-    pos  <- vec > 0
-    vec[negs][i] <- vec[negs][i <- sample(sum(negs), 1)] + 1
-    vec[pos][i]  <- vec[pos ][i <- sample(sum(pos ), 1)] - 1
-  }
-  return(vec)
-}
--- R/sample.vec.R
+++ R/sample.vec.R
@@ -1,25 +1,11 @@
-# https://stackoverflow.com/questions/13990125/sample-from-vector-of-varying-length-including-1
-# QQQQ: https://stackoverflow.com/questions/13990125/sample-from-vector-of-varying-length-including-1
-#
-# I have a vector with varying length, which can sometimes be of length 1.
-#
-# I would like to sample from this vector such that if its length is 1 it always samples that 1 number.
-#
-# sample() won't do this because it samples from 1 to the digit when sample size is 1.
-#
-# AAAA
-# This is a documented feature:
-#
-#     If x has length 1, is numeric (in the sense of is.numeric) and x >= 1, sampling via sample takes place from 1:x. Note that this convenience feature may lead to undesired behaviour when x is of varying length in calls such as sample(x).
-#
-# An alternative is to write your own function to avoid the feature:
-#
-# sample.vec <- function(x, ...) x[sample(length(x), ...)]
-# sample.vec(10)
-# # [1] 10
-# sample.vec(10, 3, replace = TRUE)
-# # [1] 10 10 10
-#
-# Some functions with similar behavior are listed under seq vs seq_along. When will using seq cause unintended results?
-
+#' Samples n elements from vector x even if x is size 1 (https://stackoverflow.com/questions/13990125/sample-from-vector-of-varying-length-including-1)
+#' @name sample.vec
+#' @usage sample.vec(x,n)
+#' @param x vector to sample from
+#' @param size a non-negative integer giving the number of items to choose.
+#' @param replace	should sampling be with replacement? (default=FALSE)
+#' @param prob a vector of probability weights for obtaining the elements of the vector being sampled.
+#' @param useHash	logical indicating if the hash-version of the algorithm should be used. Can only be used for replace = FALSE, prob = NULL, and size <= n/2, and really should be used for large n, as useHash=FALSE will use memory proportional to n.
+#' @export
 sample.vec <- function(x, ...) x[sample(length(x), ...)]
+
